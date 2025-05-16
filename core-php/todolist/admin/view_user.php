2d09976b0cbe22d4d38018a4f8d75b162243d797
milansir <rwn3developer11@gmail.com>
2025-05-16 16:35:42 +0530

    admin side role and status change task successfully update

--- core-php/todolist/admin/view_user.php
+++ core-php/todolist/admin/view_user.php
@@ -1,6 +1,43 @@
 <?php
     include('checkAdmin.php');
     include('db.php');
+
+    //user status change active , deactive
+    if(isset($_GET['status']) && isset($_GET['id'])){
+        $id = $_GET['id'];
+        $status = $_GET['status'];
+        if($status == 1){
+            $qu = "UPDATE `users` SET `status`='0' WHERE `id`='$id'";
+            $res = mysqli_query($con,$qu);
+            if($res > 0){
+                $_SESSION['message'] = "status successfully change";
+            }else{
+                $_SESSION['message'] = "status role successfully change";
+            }
+        }else{
+            $qu = "UPDATE `users` SET `status`='1' WHERE `id`='$id'";
+            $res = mysqli_query($con,$qu);
+            if($res > 0){
+                $_SESSION['message'] = "status successfully change";
+            }else{
+                $_SESSION['message'] = "status role successfully change";
+            }
+        }
+    }
+
+    //user role update 
+    if(isset($_POST['update_role'])){
+        $role_edit_id = $_POST['role_edit_id'];
+        $changerole = $_POST['change_role'];
+        $qu = "UPDATE `users` SET `role`='$changerole' WHERE `id`='$role_edit_id'";
+        $res = mysqli_query($con,$qu);
+        if($res > 0){
+            $_SESSION['message'] = "user role successfully change";
+        }else{
+             $_SESSION['message'] = "role not successfully change";
+        }
+    }
+
     include('header.php'); 
 ?>
 
@@ -35,6 +72,14 @@
                 <!-- ============================================================== -->
                 <div class="row">
                     <div class="col-12">
+                        <?php 
+                            if(isset($_SESSION['message'])){
+                        ?>
+                        <div class="alert alert-success" role="alert">
+                                <?php echo $_SESSION['message'];  ?>
+                        </div>
+                        <?php } ?>
+                        <?php unset($_SESSION['message']); ?>
                         <div class="card">
                             <div class="card-body">
                                 <h5 class="card-title m-b-0">Show all user</h5>
@@ -42,10 +87,14 @@
                             <table class="table">
                                   <thead>
                                     <tr>
-                                      <th scope="col">#</th>
-                                      <th scope="col">First</th>
-                                      <th scope="col">Last</th>
-                                      <th scope="col">Handle</th>
+                                      <th scope="col">Id</th>
+                                      <th scope="col">Name</th>
+                                      <th scope="col">Email</th>
+                                      <th scope="col">Password</th>
+                                      <th scope="col">Role</th>
+                                      <th scope="col">Change role</th>
+
+                                      <th scope="col">Status</th>
                                     </tr>
                                   </thead>
                                   <tbody>
@@ -61,6 +110,32 @@
                                                 <td><?php echo $row['name']; ?></td>
                                                 <td><?php echo $row['email'];?></td>
                                                 <td><?php echo $row['password'];?></td>
+                                                <td>
+                                                    <?php if($row['role']==1) { ?>
+                                                        <a href=""  class="btn btn-info btn-sm">admin</a>
+                                                    <?php } else { ?>
+                                                        <a href=""  class="btn btn-secondary btn-sm">user</a>
+                                                    <?php } ?>   
+                                                </td>
+                                                <td>
+                                                    <form method="post" action="">
+                                                        <input type="hidden" name="role_edit_id" value="<?php echo $row['id']; ?>">
+                                                        <select class="form-control w-50" name="change_role">
+                                                            <option value="">---select role---</option>
+                                                            <option <?php if($row['role']==1) echo "selected"; ?> value="1">admin</option>
+                                                            <option <?php if($row['role']==0) echo "selected"; ?> value="0">user</option>
+                                                        </select><br>
+                                                        <input type="submit" name="update_role" class="btn btn-primary btn-sm"/>
+                                                    </form>
+                                                </td>
+                                                <td>
+                                                    <?php if($row['status']==1) { ?>
+                                                        <a href="view_user.php?status=<?php echo $row['status'] ?>&id=<?php echo $row['id'] ?>"  class="btn btn-success btn-sm">active</a>
+                                                    <?php } else { ?>
+                                                        <a href="view_user.php?status=<?php echo $row['status'] ?>&id=<?php echo $row['id'] ?>"  class="btn btn-danger btn-sm">user</a>
+                                                    <?php } ?>
+                                                    
+                                                </td>
                                             </tr>
                                         <?php } ?>
                                     
