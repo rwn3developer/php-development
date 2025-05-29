469b1f52d54bf717e760c16295130ea5a8780dcd
milansir <rwn3developer11@gmail.com>
2025-05-29 15:15:37 +0530

    categori crud complete

--- ecommerce/admin/add_categories.php
+++ ecommerce/admin/add_categories.php
@@ -4,24 +4,49 @@
     
 
     $categories = '';
+    $msg = '';
     if(isset($_GET['id']) &&  $_GET['id']!=''){
         $id = $_GET['id'];
         $sql = "SELECT * FROM `categories` WHERE `id`='$id'";
         $res = mysqli_query($con,$sql);
-        $row = mysqli_fetch_array($res);
-        $categories = isset($row['categories']) ? $row['categories'] : '';
+
+        $check = mysqli_num_rows($res);
+        
+        if($check > 0){
+             $row = mysqli_fetch_array($res);
+             $categories = isset($row['categories']) ? $row['categories'] : '';
+        }else{
+            header('location:categories.php');
+            die();
+        }
+
+       
     }
 
     if(isset($_POST['submit'])){
         $categories = $_POST['categories'];
 
-        if(isset($_GET['id']) && $_GET['id']!=''){
-            $sql = "UPDATE `categories` SET `categories`='$categories' WHERE `id`='$id'";
-        }else{
-            $sql = "INSERT INTO `categories` (`categories`,`status`) VALUES ('$categories','1')";
-        }
-        $res = mysqli_query($con,$sql);
-        header('location:categories.php');
+         $sql = "SELECT * FROM `categories` WHERE `categories`='$categories'";
+        
+         $res = mysqli_query($con,$sql);
+
+         $check = mysqli_num_rows($res);
+        //  echo $check;
+         if($check > 0){
+            $msg = "Category already exists";
+         }else{
+            if(isset($_GET['id']) && $_GET['id']!=''){
+                $sql = "UPDATE `categories` SET `categories`='$categories' WHERE `id`='$id'";
+            }else{
+                $sql = "INSERT INTO `categories` (`categories`,`status`) VALUES ('$categories','1')";
+            }
+             $res = mysqli_query($con,$sql);
+         }
+         echo "<script>
+                    setTimeout(()=>{
+                        window.location.href='categories.php'; 
+                    },2000);
+                </script>";
     }
     require('header.inc.php'); 
 ?>
@@ -30,11 +55,13 @@
  <div class="content">
             <div class="animated fadeIn">
                 <div class="row">
-                    
+                   
                     <div class="col-lg-6 col-md-8 col-sm-12">
+                         
                         <div class="card">
                             <div class="card-header">
                                 <strong class="card-title">Add Categories</strong>
+                               
                             </div>
                             <div class="card-body">
                                 <form method="post">
@@ -46,15 +73,14 @@
                                     
                                     <button type="submit" name="submit" class="btn btn-primary">Submit</button>
                                 </form>
+                                <hr>
+                                <?php
+                                    if(isset($msg)){ ?>
+                                        <p><?php echo $msg; ?></p>
+                                    <?php } ?>
                             </div>
                         </div>
                     </div>
-
-               
-
-                
-
-               
             </div>
 
 
---
+++ ecommerce/admin/contact_us.php
@@ -0,0 +1,68 @@
+<?php 
+    
+    require('connection.inc.php');
+    require('header.inc.php'); 
+
+    
+    
+?>
+
+
+ <div class="content">
+            <div class="animated fadeIn">
+                <div class="row">
+                    
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <strong class="card-title">Contact Us</strong>
+                            </div>
+                            <div class="card-body">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                        <th scope="col">#</th>
+                                          <th scope="col">Id</th>
+                                          <th scope="col">Name</th>
+                                          <th scope="col">Email</th>
+                                          <th scope="col">Mobile</th>
+                                          <th scope="col">Query</th>
+                                          <th scope="col">Date</th>
+                                      </tr>
+                                  </thead>
+                                  <tbody>
+                                    <?php
+                                        $i=1;
+                                        $sql = "SELECT * FROM `contact_us`";
+                                        $res = mysqli_query($con,$sql);
+                                        while($row = mysqli_fetch_array($res)) { 
+                                    ?>
+                                        <tr>
+                                            <td><?php echo $i++; ?></td>
+                                            <td><?php echo $row['id']; ?></td>
+                                            <td><?php echo $row['name']; ?></td>
+                                            <td><?php echo $row['email']; ?></td>
+                                            <td><?php echo $row['mobile']; ?></td>
+                                            <td><?php echo $row['comment']; ?></td>
+                                            <td><?php echo $row['added_on']; ?></td>
+
+
+                                           
+
+                                            
+                                        </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- .animated -->
+</div><!-- .content -->
+
+<div class="clearfix"></div>
+
+
+<?php require('footer.inc.php'); ?>
